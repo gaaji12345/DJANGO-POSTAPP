750640554acbd909f18bee6f676a63bc84d511ad
gaji12345 <gajimax12345@gmail.com>
2024-12-20 12:12:49 +0530

    add about page

--- djangoPost1/urls.py
+++ djangoPost1/urls.py
@@ -19,8 +19,14 @@ from tkinter.font import names
 from django.contrib import admin
 from django.urls import path
 from .import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home_page,name='home')
+    path('',views.home_page,name='home'),
+    path('about/',views.about_page,name='about')
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
